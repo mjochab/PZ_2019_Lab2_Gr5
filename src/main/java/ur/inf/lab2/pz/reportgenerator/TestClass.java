6e88d85bd27896aa34697c4eb1f6d34fa72c9638
irek140 <irek140@poczta.fm>
2019-05-16 17:56:31 +0200

    design and create a pdf report style   I19L2GR6-46

--- src/main/java/ur/inf/lab2/pz/reportgenerator/TestClass.java
+++ src/main/java/ur/inf/lab2/pz/reportgenerator/TestClass.java
@@ -7,8 +7,8 @@ public class TestClass {
     public static void main(String[] args) throws JRException {
 
 
-        TaskReportImpl report = new TaskReportImpl("a","b","c",
-                "d","e","f","g","h","j","k");
-        TaskReportGenerator.generateTaskReport(report,"/home/damian/Pobrane/output.pdf");
+        TaskReportImpl report = new TaskReportImpl("Przykladowa godnosc pracownika","Przykladowy tytul zadania","Przykladowy dodatkowy opis zadania",
+                "Przykladowe imie klienta","Przykladowe nazwisko klienta","Przykladowy nr telefonu","Przykladowa ulica","Przykladowy nr domu","Przykladowy nr mieszkania","Przykladowe miasto");
+        TaskReportGenerator.generateTaskReport(report,"/home/irek/Pobrane/output.pdf");
     }
 }
