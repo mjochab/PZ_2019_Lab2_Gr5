964600e131448e85a4e867b5e31a7bce2d844f27
Patryk Kleczkowski <patryk.kleczkowski.96@gmail.com>
2019-05-16 21:06:37 +0200

    Backend - fix relation between manager and serviceman

--- src/main/java/ur/inf/lab2/pz/servicemanmanagement/services/EmployeeService.java
+++ src/main/java/ur/inf/lab2/pz/servicemanmanagement/services/EmployeeService.java
@@ -1,24 +1,23 @@
 package ur.inf.lab2.pz.servicemanmanagement.services;
 
 import javafx.scene.text.Text;
-import org.springframework.stereotype.Service;
-import ur.inf.lab2.pz.servicemanmanagement.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import ur.inf.lab2.pz.servicemanmanagement.repository.RoleRepository;
-import ur.inf.lab2.pz.servicemanmanagement.repository.UserRepository;
-import ur.inf.lab2.pz.servicemanmanagement.services.EmailSender;
-import ur.inf.lab2.pz.servicemanmanagement.services.EncryptionService;
-import ur.inf.lab2.pz.servicemanmanagement.domain.enums.Roles;
+import org.springframework.stereotype.Service;
+import ur.inf.lab2.pz.servicemanmanagement.domain.SecurityContext;
+import ur.inf.lab2.pz.servicemanmanagement.domain.Serviceman;
+import ur.inf.lab2.pz.servicemanmanagement.domain.User;
 import ur.inf.lab2.pz.servicemanmanagement.domain.dto.workerAddDTO;
+import ur.inf.lab2.pz.servicemanmanagement.domain.enums.Roles;
+import ur.inf.lab2.pz.servicemanmanagement.repository.AllUsersRepository;
+import ur.inf.lab2.pz.servicemanmanagement.repository.RoleRepository;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @Service
 public class EmployeeService {
 
     @Autowired
-    private UserRepository userRepository;
+    private AllUsersRepository allUsersRepository;
 
     @Autowired
     private EncryptionService encryptionService;
@@ -29,18 +28,20 @@ public class EmployeeService {
     @Autowired
     private RoleRepository roleRepository;
 
-    public void addWorker(workerAddDTO dto, Text existingUserAlert)  {
+    public void addWorker(workerAddDTO dto, Text existingUserAlert) {
 
         existingUserAlert.setVisible(false);
 
-        if(userRepository.findAllByEmail(dto.getEmail()).isEmpty()){
+        if (allUsersRepository.findAllByEmail(dto.getEmail()).isEmpty()) {
+
+            User manager = SecurityContext.getLoggedUser();
 
             String firstPassword = UUID.randomUUID().toString().substring(0, 6);
-            User user = new User(dto.getEmail(), encryptionService.encode(firstPassword), dto.getGroupName());
-            user.setRole(roleRepository.findByRole(Roles.ROLE_SERVICEMAN.toString()));
-            userRepository.save(user);
+            Serviceman serviceman = new Serviceman(dto.getEmail(), encryptionService.encode(firstPassword), dto.getGroupName(), manager);
+            serviceman.setRole(roleRepository.findByRole(Roles.ROLE_SERVICEMAN.toString()));
+            allUsersRepository.save(serviceman);
             emailSender.sendEmail(dto.getEmail(), "Account's First password", firstPassword);
 
         } else existingUserAlert.setVisible(true);
-        }
+    }
 }
