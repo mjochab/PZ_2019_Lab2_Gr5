cb37d7c90bdf116c49dde8e8528597883ded3fe9
DarekNiemczycki <dariuszniemczycki@gmail.com>
2019-05-20 11:05:35 +0200

    change data to manager

--- src/main/java/ur/inf/lab2/pz/servicemanmanagement/controller/ManagerDataController.java
+++ src/main/java/ur/inf/lab2/pz/servicemanmanagement/controller/ManagerDataController.java
@@ -1,15 +1,29 @@
 package ur.inf.lab2.pz.servicemanmanagement.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ur.inf.lab2.pz.servicemanmanagement.domain.dto.ManagerDataDTO;
+import ur.inf.lab2.pz.servicemanmanagement.services.ManagerDataService;
 import ur.inf.lab2.pz.servicemanmanagement.view.ViewManager;
 
 import java.io.IOException;
 
 @Controller
 public class ManagerDataController {
+
+    @Autowired
+    private ManagerDataService managerDataService;
+
+    @FXML
+    private JFXPasswordField passwordField, confirmPassField;
+
+    @FXML
+    private JFXTextField nameField, lastNameField, companyNameField;
+
     private ViewManager viewManager;
 
 //    public void submitData(ActionEvent event) throws IOException {
@@ -20,7 +34,9 @@ public class ManagerDataController {
 
     @FXML
     public void submitManagerData() {
-
+        ManagerDataDTO dto = new ManagerDataDTO(nameField.getText(), lastNameField.getText(), companyNameField.getText(),
+                passwordField.getText(), confirmPassField.getText());
+        managerDataService.submitManagerData(dto);
     }
 
 
---
+++ src/main/java/ur/inf/lab2/pz/servicemanmanagement/domain/dto/ManagerDataDTO.java
@@ -0,0 +1,45 @@
+package ur.inf.lab2.pz.servicemanmanagement.domain.dto;
+
+public class ManagerDataDTO {
+
+    String firstName;
+    String lastName;
+    String companyName;
+    String password;
+    String confirmPassword;
+
+
+    public ManagerDataDTO(String firstName, String lastName, String companyName, String password, String confirmPassword) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.companyName = companyName;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() { return lastName;}
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public  String getCompanyName() { return companyName; }
+
+    public void setCompanyName(String companyName) { this.companyName = companyName; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) {this.password = password; }
+
+    public String getConfirmPassword() { return confirmPassword; }
+
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
+
+}
+
