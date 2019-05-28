a97eb986c89ea17fbb9e626a4b39fd63e96d8b8e
kijko <ae86trueno>
2019-05-28 14:12:33 +0200

    hotfix5

--- src/main/java/ur/inf/lab2/pz/servicemanmanagement/config/reportgenerator/TaskReportGenerator.java
+++ src/main/java/ur/inf/lab2/pz/servicemanmanagement/config/reportgenerator/TaskReportGenerator.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class TaskReportGenerator {
 
     public void generateTaskReport(TaskReport task, String pathToFile) throws JRException {
-        pathToFile += "/raport_" + new Date().getTime();
+        pathToFile += "/raport_" + new Date().getTime() + ".pdf";
 
         JasperReport jasperReport = JasperCompileManager.compileReport(getClass().getClassLoader().getResourceAsStream("/jrxml/report.jrxml"));
 
